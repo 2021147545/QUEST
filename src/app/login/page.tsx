a1b5477bfd6265710c7d371a9fb86174c824c1a0
2021147545 <101983778+2021147545@users.noreply.github.com>
2025-06-09 17:35:55 +0900

    mbtest8

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -65,7 +65,7 @@ export default function LoginPage() {
     if (!newFeedback.trim()) return;
 
     const data = {
-      username: username,
+      username: "비회원",
       feedback: newFeedback
     };
 
